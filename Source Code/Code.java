1b7879109e0de4a04562c62353403112d8d5e48d
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-04-28 14:13:45 +0530

    Added storage cost as weight to the edges of CTG

--- Source Code/Code.java
+++ Source Code/Code.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Code
@@ -63,13 +67,13 @@ public class Code
 	{
 		int V;
 		int E;
-		int[][] adjMatrix;
+		double[][] adjMatrix;
 		
 		public CTG(int nodes)
 		{
 			V = 2*nodes+2;
 			E = 0;
-			adjMatrix = new int[V][V];
+			adjMatrix = new double[V][V];
 			
 			int n = nodes;
 			String[] arr = new String[2*n+2];
@@ -89,7 +93,7 @@ public class Code
 			
 			for(int x=1;x<(arr.length-1);x++)
 			{
-				addEdge(0,x);    // edge from START vertex to all other vertices
+				addEdge(0,x,arr);    // edge from START vertex to all other vertices except END
 				
 				/*for(int y=x+1;y<(arr.length)/2;y++)
 				{
@@ -100,39 +104,115 @@ public class Code
 				{
 					if(arr[x].substring(0,1)!=arr[m].substring(0,1)&&Integer.parseInt(arr[x].substring(0,1))<Integer.parseInt(arr[m].substring(0,1)))
 					{
-						addEdge(x,m);
+						addEdge(x,m,arr);
 					}
 				}
 				
-				addEdge(0,arr.length-1);    // edge from START vertex to END vertex
-				addEdge(x,arr.length-1);    // edge from all other vertices to END vertex
+				//addEdge(0,arr.length-1,arr);    // edge from START vertex to END vertex
+				//addEdge(x,arr.length-1,arr);    // edge from all other vertices to END vertex
 			}
 			
 			toString(arr);
 		}
 		
-		public void addEdge(int arr, int arr2)
+		public void addEdge(int fromVertex, int toVertex, String[] array)
 		{
-			adjMatrix[arr][arr2] = 1;
+			adjMatrix[fromVertex][toVertex] = 1;
 			E++;
+			CalculateWeight(fromVertex,toVertex, array);
+		}
+		
+		public void CalculateWeight(int a, int b, String[] array)
+		{
+			double WeightOfEdge = 0 ;
+			
+			// WeightOfEdge = Storage cost of "b" + regeneration cost of all vertices between "a" and "b"
+			
+			String s = array[b];   // 3ver2   ///   x = 2 y=3
+			int x = Integer.parseInt(s.substring(s.length()-1, s.length()));
+			int y = Integer.parseInt(s.substring(0,1));
+			
+			try 
+            {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/minor2", "root", "");
+                
+                String query1 = null;
+                
+                if(x==1)
+                {
+                	query1 = "SELECT Storage_Cost from cloud1 WHERE File_Number='"+y+"'";
+                }
+                if(x==2)
+                {
+                	query1 = "SELECT Storage_Cost from cloud2 WHERE File_Number='"+y+"'";
+                }
+                //String query1 = "SELECT Storage_Cost from cloud'"+x+"' WHERE File_Number='"+y+"'";
+                Statement sta1 = connection.createStatement();
+                ResultSet rs = sta1.executeQuery(query1); 
+                
+                rs.next();
+                WeightOfEdge = rs.getDouble(1);//Double.parseDouble(rs.toString());
+                
+                connection.close();
+            }
+            catch (Exception exception) 
+            {
+            	exception.printStackTrace();
+            }
+			
+			
+			// Now calculating regeneration cost of vertices in between
+			
+			
+			
+			
+			
+			
+			adjMatrix[a][b] = WeightOfEdge;
 		}
 		
 		public void toString(String arr[])
 		{
 			System.out.println("\n\nCTG : ");
 			System.out.println("\n" + V + " vertices, "+ E + " edges "+"\n");
-			System.out.print("        ");
+			System.out.print("------|--");
+			for(int p=1;p<=10;p++)
+			{
+				System.out.print("-----------|");
+			}
+			System.out.print("\n");
+			System.out.print("      |  ");
 			for(int j=0;j<arr.length;j++)
 			{
-				System.out.print(arr[j] + "  ");
+				System.out.print("  ");
+				System.out.print(arr[j] + "    |");
+			}
+			System.out.print("\n");
+			System.out.print("------|--");
+			for(int p=1;p<=10;p++)
+			{
+				System.out.print("-----------|");
 			}
 			System.out.print("\n");
 			for(int j=0;j<arr.length;j++)
 			{
-				System.out.print(arr[j] + " :   ");
-				for(int w:adjMatrix[j])
+				System.out.print(arr[j] + " |  ");
+				for(double w:adjMatrix[j])
 				{
-					System.out.print(w+"      ");
+					System.out.print(w);
+					String space = String.valueOf(w);
+					int x = 11 - space.length();
+					for(int i = 1;i<=x;i++)
+					{
+						System.out.print(" ");
+					}
+					System.out.print("|");
+				}
+				System.out.print("\n");
+				System.out.print("------|--");
+				for(int p=1;p<=10;p++)
+				{
+					System.out.print("-----------|");
 				}
 				System.out.print("\n");
 			}
