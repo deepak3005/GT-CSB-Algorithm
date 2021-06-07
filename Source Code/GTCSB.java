a218ed8bfaed5aa913a45a378f4f009ee882249a
deepak3005 <61706251+deepak3005@users.noreply.github.com>
2021-06-07 12:08:53 +0530

    Update GTCSB.java

--- Source Code/GTCSB.java
+++ Source Code/GTCSB.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
@@ -59,6 +60,59 @@ public class GTCSB extends JFrame {
 			}
 		});
 	}
+	
+	public void WantToDeleteFile(int fn, int cn)
+	{
+		int a=JOptionPane.showConfirmDialog(null, "For cost reduction, you should delete file:"+fn+" from cloud:"+cn+". Do you want to delete the file?");  
+		
+		if(a==JOptionPane.YES_OPTION)
+		{  
+			DeleteFile(fn, cn);
+		    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		}  
+	}
+	
+	public void DeleteFile(int fn, int cn)
+	{
+		if(cn==1)
+		{
+			try 
+	        {
+	        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/minor2", "root", "");
+	               
+	            String query2 = "DELETE FROM cloud1 WHERE File_Number='"+fn+"'";
+
+	            Statement sta = connection.createStatement();
+	            sta.executeUpdate(query2);
+	            
+	            connection.close();
+	        }
+	        catch (Exception exception) 
+	        {
+	            exception.printStackTrace();
+	        }
+		}
+		if(cn==2)
+		{
+			try 
+	        {
+	        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/minor2", "root", "");
+	               
+	            String query2 = "DELETE FROM cloud2 WHERE File_Number='"+fn+"'";
+
+	            Statement sta = connection.createStatement();
+	            sta.executeUpdate(query2);
+	            
+	            connection.close();
+	        }
+	        catch (Exception exception) 
+	        {
+	            exception.printStackTrace();
+	        }
+		}
+		
+		JOptionPane.showMessageDialog(null, new JLabel("File Number "+fn+" successfully deleted from Cloud"+cn+" server.", JLabel.CENTER), "Output", JOptionPane.PLAIN_MESSAGE);
+	}
 
 	/**
 	 * Create the frame.
@@ -81,29 +135,29 @@ public class GTCSB extends JFrame {
 		txtGtcsbMinimumCost = new JTextField();
 		txtGtcsbMinimumCost.setText("GT-CSB  Minimum  Cost  Algorithm");
 		txtGtcsbMinimumCost.setForeground(Color.WHITE);
-		txtGtcsbMinimumCost.setFont(new Font("Cambria", Font.BOLD, 30));
+		txtGtcsbMinimumCost.setFont(new Font("Cambria", Font.BOLD, 36));
 		txtGtcsbMinimumCost.setEditable(false);
 		txtGtcsbMinimumCost.setColumns(10);
 		txtGtcsbMinimumCost.setBorder(null);
 		txtGtcsbMinimumCost.setBackground(Color.DARK_GRAY);
-		txtGtcsbMinimumCost.setBounds(455, 65, 480, 40);
+		txtGtcsbMinimumCost.setBounds(365, 65, 570, 50);
 		contentPane.add(txtGtcsbMinimumCost);
 		
 		textField_2 = new JTextField();
 		textField_2.setText("Cloud Service Provider");
 		textField_2.setForeground(Color.WHITE);
-		textField_2.setFont(new Font("Cambria", Font.BOLD, 17));
+		textField_2.setFont(new Font("Cambria", Font.BOLD, 22));
 		textField_2.setEditable(false);
 		textField_2.setColumns(10);
 		textField_2.setBorder(null);
 		textField_2.setBackground(Color.DARK_GRAY);
-		textField_2.setBounds(300, 150, 185, 35);
+		textField_2.setBounds(475, 160, 240, 35);
 		contentPane.add(textField_2);
 		
 		final JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Cloud 1", "Cloud 2"}));
-		comboBox.setFont(new Font("Cambria", Font.BOLD, 15));
-		comboBox.setBounds(511, 150, 84, 30);
+		comboBox.setFont(new Font("Cambria", Font.BOLD, 18));
+		comboBox.setBounds(727, 160, 95, 30);
 		contentPane.add(comboBox);
 		
 		JButton btnNewButton = new JButton("Get Data From Cloud");
@@ -213,10 +267,10 @@ public class GTCSB extends JFrame {
 		});
 		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setFont(new Font("Cambria", Font.BOLD, 17));
+		btnNewButton.setFont(new Font("Cambria", Font.BOLD, 22));
 		btnNewButton.setBorder(null);
 		btnNewButton.setBackground(new Color(65, 105, 225));
-		btnNewButton.setBounds(265, 200, 350, 35);
+		btnNewButton.setBounds(460, 215, 380, 50);
 		contentPane.add(btnNewButton);
 		
 		JButton btnRunGtcsbMin = new JButton("Run GT-CSB Min Cost Algorithm");
@@ -239,26 +293,13 @@ public class GTCSB extends JFrame {
 		});
 		btnRunGtcsbMin.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnRunGtcsbMin.setForeground(Color.WHITE);
-		btnRunGtcsbMin.setFont(new Font("Cambria", Font.BOLD, 17));
+		btnRunGtcsbMin.setFont(new Font("Cambria", Font.BOLD, 22));
 		btnRunGtcsbMin.setBorder(null);
 		btnRunGtcsbMin.setBackground(new Color(65, 105, 225));
-		btnRunGtcsbMin.setBounds(265, 252, 350, 35);
+		btnRunGtcsbMin.setBounds(460, 290, 380, 50);
 		contentPane.add(btnRunGtcsbMin);
 		
-		JButton btnNormalVsGtcsb = new JButton("Normal VS GT-CSB Cost Comparison Chart");
-		btnNormalVsGtcsb.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNormalVsGtcsb.setForeground(Color.WHITE);
-		btnNormalVsGtcsb.setFont(new Font("Cambria", Font.BOLD, 17));
-		btnNormalVsGtcsb.setBorder(null);
-		btnNormalVsGtcsb.setBackground(new Color(65, 105, 225));
-		btnNormalVsGtcsb.setBounds(265, 305, 350, 35);
-		contentPane.add(btnNormalVsGtcsb);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setFont(new Font("Cambria", Font.BOLD, 15));
-		textArea.setEditable(false);
-		textArea.setBounds(685, 150, 339, 190);
-		contentPane.add(textArea);
 		
 		scrollPane = new JScrollPane();
 		scrollPane.setBounds(93, 380, 1114, 210);
